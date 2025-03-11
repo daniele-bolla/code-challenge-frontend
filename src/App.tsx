58898d717171071210229c6523873f09df1c8098
bolladaniele <bolladaniele@gmail.com>
2025-03-11 18:41:35 +0000

    install and init  mapbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
+import { useRef, useEffect } from 'react'
+import mapboxgl from 'mapbox-gl'
+
+import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.css'
 
 function App() {
+  const mapContainer = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    mapboxgl.accessToken = "pk.eyJ1IjoidHRjLWhheWVza2ciLCJhIjoiY203dWNoZG94MDIyYzJxcjZ6Y2EwY3BybyJ9.Jn4YpF1DPcTaVmqN-uyJxg";
+
+    if (mapContainer.current) {
+      const map = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: "mapbox://styles/mapbox/dark-v11",
+        center: [-74.0060152, 40.7127281],
+        zoom: 5,
+        maxZoom: 15,
+      });
+
+      // Add zoom controls
+      map.addControl(new mapboxgl.NavigationControl(), "top-left");
+
+      // Add your custom markers and lines here
+
+      // Clean up on unmount
+      return () => map.remove();
+    }
+  }, []);
   return (
     <>
       <h1>Worn Tyres Platform</h1>
+      <div id='map-container' ref={mapContainer}/>
     </>
   )
 }
