ac55970f65c9de738e82f0b9847b6df0324637b8
bolladaniele <bolladaniele@gmail.com>
2025-03-12 12:57:35 +0000

    serve images

--- backend-data/server.ts
+++ backend-data/server.ts
@@ -3,12 +3,18 @@ import cors from 'cors';
 import fleetRoutes from './api/fleets';
 import vehicleRoutes from './api/vehicles';
 import snapshotRoutes from './api/snapshots';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, './public')));
 
 // Routes
 app.use('/fleets', fleetRoutes);
